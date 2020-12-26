1f171e6c9be16a98a1ef6ba015c3e06b7f56df6a
kjammes <jayeshkarli@gmail.com>
2020-12-26 16:54:57 +0530

    Completed RailFenceCipher

--- PRAC3RailFence/RailFence.java
+++ PRAC3RailFence/RailFence.java
@@ -1,25 +1,57 @@
 import java.io.*;
+import java.util.*;
 
 class RailFence {
-  public static encipher(String text,int key){
+  public static StringBuilder encipher(String text,int key){
+    StringBuilder etext = new StringBuilder("");
     char[][] RailFence = new char[key][text.length()];
     int row = 0;
+    boolean inc = false;
     for( int col = 0 ; col<text.length() ; col++ ){
-      if( row==key-1 )
-        row = 0;
+      if( row==key-1 || row==0 )
+        inc = !inc;
       
-      RailFence[row++][col] = text.charAt(col);
+      RailFence[row][col] = text.charAt(col);
       row = inc ? row+1 : row-1;
     }
-    for( int[] curRow:RailFence ){
-      for(int el:curRow){
-        if(  )
+    for( char[] curRow:RailFence ){
+      for(char el:curRow){
+        if( el!='\u0000' )
+          etext.append(el);
       }
     }
+    return etext;
   }
 
-  public static decipher(String etext,int key){
+  public static StringBuilder decipher(String etext,int key){
+    StringBuilder text = new StringBuilder("");
+    char[][] RailFence = new char[key][etext.length()];
+    int row = 0;
+    boolean inc = false;
+    for( int col = 0 ; col<etext.length() ; col++ ){
+      if( row==key-1 || row==0 )
+        inc = !inc;
+      
+      RailFence[row][col] = '*';
+      row = inc ? row+1 : row-1;
+    }
+    int pointer = 0;
+    for( int curRow=0 ; curRow<key ; curRow++ ){
+      for( int col=0 ; col<etext.length() ; col++ ){
+        if( RailFence[curRow][col]=='*' )
+          RailFence[curRow][col] = etext.charAt(pointer++);
+      }
+    }
 
+    row = 0;
+    inc = false;
+    for( int col = 0 ; col<etext.length() ; col++ ){
+      if( row==key-1 || row==0 )
+        inc = !inc;
+      text.append(RailFence[row][col]);
+      row = inc ? row+1 : row-1;
+    }
+    return text;
   }
 
   public static void main(String args[]){
